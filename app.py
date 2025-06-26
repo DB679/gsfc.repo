ac03c1e9e4844886f423cc0bd4c6c77205d61ddf
DB679 <devbahtt16@yahoo.com>
2025-06-26 18:16:56 +0530

    Updated app with new features

--- app.py
+++ app.py
@@ -216,4 +216,5 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT if available
+    app.run(debug=False, host='0.0.0.0', port=port)
